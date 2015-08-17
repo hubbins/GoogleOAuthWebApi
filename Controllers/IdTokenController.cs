c6e93c6d52ca3fc5ca5e0029340ab7d9b6248315
Sean OConnor <soconnor@yahoo.com>
2015-08-16 20:40:44 -0500

    First commit

---
+++ Controllers/IdTokenController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GoogleOAuthWebApi.Controllers
+{
+    public class IdTokenController : ApiController
+    {
+        public IHttpActionResult SetToken([FromBody] string idToken)
+        {
+            string jsonResponse;
+            using (var client = new WebClient())
+            {
+                jsonResponse = client.DownloadString("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + idToken);
+            }
+
+            JObject user = JObject.Parse(jsonResponse);
+            // verify aud
+            // sub is user id
+
+            return Ok();
+        }
+    }
+}
